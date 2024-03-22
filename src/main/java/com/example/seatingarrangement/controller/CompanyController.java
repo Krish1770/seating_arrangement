eee645c5eedf5732a63f26ef855396e26044be5e
krish1770 <ramesh.krishnaprasath@divum.in>
2024-03-22 22:27:34 +0530

    final commit

--- src/main/java/com/example/seatingarrangement/controller/CompanyController.java
+++ src/main/java/com/example/seatingarrangement/controller/CompanyController.java
@@ -5,24 +5,30 @@ import com.example.seatingarrangement.dto.CompanyDto;
 import com.example.seatingarrangement.dto.LayoutDto;
 import com.example.seatingarrangement.dto.ResponseDto;
 import com.example.seatingarrangement.repository.CompanyRepository;
+import com.example.seatingarrangement.service.CompanyService;
 import org.apache.coyote.BadRequestException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CompanyController  implements CompanyApi {
 
+    @Autowired
+    private CompanyService companyService;
 
     @Override
     public ResponseEntity<ResponseDto> getAllLayOut(String companyName) {
-        return null;
+        return companyService.getAllLayOut(companyName);
     }
 
     @Override
     public ResponseEntity<ResponseDto> updateLayout(LayoutDto layoutDto) throws BadRequestException {
-        return null;
+        return companyService.updateLayout(layoutDto);
     }
 
     @Override
     public ResponseEntity<ResponseDto> add(CompanyDto companyDto) {
-        return null;
+        return companyService.add(companyDto);
     }
 }
--- src/main/java/com/example/seatingarrangement/service/CompanyService.java
+++ src/main/java/com/example/seatingarrangement/service/CompanyService.java
@@ -5,7 +5,9 @@ import com.example.seatingarrangement.dto.LayoutDto;
 import com.example.seatingarrangement.dto.ResponseDto;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface CompanyService {
     ResponseEntity<ResponseDto> add(CompanyDto companyDto) ;
 
